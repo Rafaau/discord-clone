f9b1ff26bc2704675b8b6b8721bfce5d8216ef6b
Aleksander Michalec <amichalec999@gmail.com>
2023-04-09 23:22:02 +0200

    ATTEMPT #36

--- server/src/main.ts
+++ server/src/main.ts
@@ -20,7 +20,9 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         maxAge: 36000000,
-        domain: process.env.CLIENT_DOMAIN || 'localhost',
+        domain: process.env.COOKIE_DOMAIN || 'localhost',
+        sameSite: 'none',
+        secure: true
       },
       store: new TypeormStore().connect(sessionRepository)
     })
