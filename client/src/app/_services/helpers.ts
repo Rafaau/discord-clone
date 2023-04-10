a0898f52341651ccd99dcc00ce17d7368dc1e96e
Aleksander Michalec <amichalec999@gmail.com>
2023-04-10 11:23:33 +0200

    ATTEMPT #45

--- client/src/app/_services/helpers.ts
+++ client/src/app/_services/helpers.ts
@@ -1,15 +1,8 @@
 import { HttpHeaders } from "@angular/common/http"
 
 export class ApiHelpers {
-    static readonly headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': process.env.NG_APP_CORS_ORIGIN || 'http://localhost:4200',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': '*',
-        'Accept': 'application/json',
-        'key': 'x-api-key',
-        'Connection': 'keep-alive',
-        'value': 'NNctr6Tjrw9794gFXf3fi6zWBZ78j6Gv3UCb3y0x',
-      })
+  static readonly headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  })
 }
