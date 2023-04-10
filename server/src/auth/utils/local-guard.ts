77afa08e37b2d42aab394702b54ae9c37b76f0fc
Aleksander Michalec <amichalec999@gmail.com>
2023-04-10 11:36:38 +0200

    ATTEMPT #46

--- server/src/auth/utils/local-guard.ts
+++ server/src/auth/utils/local-guard.ts
@@ -7,6 +7,7 @@ export class LocalAuthGuard extends AuthGuard('local') {
     async canActivate(context: ExecutionContext) {
         const result = (await super.canActivate(context) as boolean)
         const request = context.switchToHttp().getRequest()
+        console.log(request)
         await super.logIn(request)
         return result
     }
@@ -16,6 +17,7 @@ export class LocalAuthGuard extends AuthGuard('local') {
 export class AuthenticatedGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<any> {
         const req = context.switchToHttp().getRequest<Request>()
+        console.log(req)
         return req.isAuthenticated()
     }
 }
